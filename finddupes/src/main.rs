a93bdcef2c0a67856726bb773d226bb59c69b103
Nathan Marley <nathan.marley@gmail.com>
2021-03-27 15:10:33 -0300

    Stub logic

--- finddupes/src/main.rs
+++ finddupes/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    if args.len() == 1 {
+        eprintln!("usage: {} <dir>", args.next().unwrap());
+        std::process::exit(1);
+    }
+
+    for filename in args.skip(1) {
+        // if filename not dir then skip
+        // open dir and read all recursively
+        // ignore '.' and '..'
+        // hash 'file' and add PATH to list => map[hash][list of paths]
+        // 'dir's should be walked
+    }
 }
